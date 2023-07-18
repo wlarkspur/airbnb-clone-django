f437957d11218066519113663c8bf4827693fcaa
wlarkspur <newacon@naver.com>
2023-07-18 23:14:50 +0200

    Rooms Admin: RoomAdmin & Amenity

--- rooms/models.py
+++ rooms/models.py
@@ -23,6 +23,7 @@ class Room(CommonModel):
             "Shared Room",
         )
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(
         max_length=50,
         default="한국",
@@ -53,6 +54,9 @@ class Room(CommonModel):
         "rooms.Amenity",
     )
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Amenity(CommonModel):
 
@@ -66,3 +70,9 @@ class Amenity(CommonModel):
         null=True,
         blank=True,
     )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
