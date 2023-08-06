5bca88bd59be3f919c8a058b6e24020351568c36
wlarkspur <newacon@naver.com>
2023-08-06 22:36:08 +0200

    views.py refactoring with APIView

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.categories),
-    path("<int:pk>", views.category),
+    path("", views.Categories.as_view()),
+    path("<int:pk>", views.CategoryDetail.as_view()),
 ]
+
+"""as_view: 하나의 규칙으로 GET, PUT 요청에 따라 코드를 실행하는 역할을 한다."""
--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -10,7 +11,54 @@ from .serializers import CategorySerializer
 그러면, 브라우저는 Queryset을 이해하지 못하기 때문에 오류가 발생한다. 대신 JSON 포맷으로 변경필요 """
 
 
-@api_view(["GET", "POST"])
+class Categories(APIView):
+    def get(self, request):
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            new_category = serializer.save()
+            return Response(
+                CategorySerializer(new_category).data,
+            )
+        else:
+            return Response(serializer.errors)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = CategorySerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+"""위 코드에서 get, put, delete 메소드 네임은 다르게 적으면 작동하지 않는다."""
+
+""" @api_view(["GET", "POST"])
 def categories(request):
     if request.method == "GET":
         all_categories = Category.objects.all()
@@ -24,10 +72,10 @@ def categories(request):
                 CategorySerializer(new_category).data,
             )
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors) """
 
 
-@api_view(["GET", "PUT", "DELETE"])
+""" @api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -50,4 +98,4 @@ def category(request, pk):
             return Response(serializer.errors)
     elif request.method == "DELETE":
         category.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+        return Response(status=HTTP_204_NO_CONTENT) """
