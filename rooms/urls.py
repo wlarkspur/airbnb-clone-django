54f650ad9428d569ab8c249210866f4e2ca591c4
wlarkspur <newacon@naver.com>
2023-08-15 00:07:05 +0200

    REST API

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>", views.AmenityDetail.as_view()),
+]
+
+
+""" urlpatterns = [
     path("", views.see_all_room),
     path("<int:room_pk>", views.see_one_room),
 ]
+ """
