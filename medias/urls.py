3dd8099a6fd4ea6003cce489633afcb5d19c2264
wlarkspur <newacon@naver.com>
2023-11-17 23:34:53 +0200

    Cloud flare uploadURL 받기

--- medias/urls.py
+++ medias/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PhotoDetail
+from .views import PhotoDetail, GetUpLoadURL
 
 urlpatterns = [
+    path("photos/get-url", GetUpLoadURL.as_view()),
     path("photos/<int:pk>", PhotoDetail.as_view()),
 ]
